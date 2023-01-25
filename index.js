89f4e76cf3909f3bbc4274b1863d31f39b71c00d
Milton Sanchez <75172873+GoldenLion94@users.noreply.github.com>
2023-01-25 13:02:17 -0500

    debugged line 5 & 6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    let buttontextContent = this.textContent;
+    let buttonTextContent = this.textContent;
 
     switch (buttonTextContent) {
       case "w":
